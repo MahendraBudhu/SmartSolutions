8228aa5595e6f13791c81b80becb166b01331a7f
MahendraBudhu <54993980+MahendraBudhu@users.noreply.github.com>
2019-10-27 01:22:50 -0400

    Adding comments

--- pingpongapp/src/main/java/com/example/pingpongapp/LoginScreen.java
+++ pingpongapp/src/main/java/com/example/pingpongapp/LoginScreen.java
@@ -16,7 +16,7 @@ public class LoginScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
     }
-
+    //Validating Login Info
     public void login(View view){
         TextView userField = (TextView) findViewById(R.id.username);    //Getting user inputs
         TextView passField = (TextView) findViewById(R.id.password);
