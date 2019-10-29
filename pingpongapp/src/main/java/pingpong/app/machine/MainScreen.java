68979213b55b10f331910c8e4eb1c5494a5d67a9
MahendraBudhu <54993980+MahendraBudhu@users.noreply.github.com>
2019-10-28 23:49:58 -0400

    Renamed the package name to pingpong.app.machine

---
+++ pingpongapp/src/main/java/pingpong/app/machine/MainScreen.java
@@ -0,0 +1,32 @@
+package pingpong.app.machine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.machine.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+    public void config(View view){ //Goes to setup screen
+        Intent intent = new Intent (this, SetupScreen.class);
+        startActivity(intent);
+    }
+
+    public void settings(View view){ //Goes to settings screen
+        Intent intent = new Intent (this, StatisticsScreen.class);
+        startActivity(intent);
+    }
+
+    public void random(View view){ //Goes to Random screen
+        Intent intent = new Intent (this, RandomScreen.class);
+        startActivity(intent);
+    }
+}
